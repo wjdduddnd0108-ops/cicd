04084eeffa8655b665047ae166fe45e23c50f40a
wjdduddnd0108-ops <wjdduddnd0108@gmail.com>
2026-06-24 17:15:22 +0900

    첫번째 업로드

---
+++ deploy-prj/src/main/java/com/kh/deploy/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.kh.deploy.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@RequestMapping("/")
+	public String home() {
+		return "Welcome to my server!";
+	}
+}
---
+++ deploy-prj/src/test/java/com/kh/deploy/DeployPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
