688b60b6ed8937d790852e51a66c5f802bd9fa84
wjdduddnd0108-ops <wjdduddnd0108@gmail.com>
2026-06-25 10:44:31 +0900

    컨트롤러 수정

--- deploy-prj/src/main/java/com/kh/deploy/controller/TestController.java
+++ deploy-prj/src/main/java/com/kh/deploy/controller/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 	@RequestMapping("/")
 	public String home() {
-		return "Welcome to my server!";
+		return "Welcome";
 	}
 }
